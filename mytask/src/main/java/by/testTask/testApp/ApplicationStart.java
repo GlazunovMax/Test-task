c2689727c1b78f884edf170782e4e2c286db123b
GlazunovMax <glazunov-1990@mail.ru>
2018-06-25 22:18:37 +0300

    Test task - Add condition

--- mytask/src/main/java/by/testTask/testApp/ApplicationStart.java
+++ mytask/src/main/java/by/testTask/testApp/ApplicationStart.java
@@ -3,6 +3,39 @@ package by.testTask.testApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/*
+ Задание:
+Система обработки заявок. 
+
+Необходимо разработать систему обработки заявок. Система должна состоять из двух модулей: клиент, админ. 
+
+Модуль клиент 
+Требования:
+- регистрировать заявки 
+- проверять статус заявки
+
+Модуль админ
+Требования:
+- получать список всех заявок
+- возможность выполнять\отказывать конкретную заявку
+- статистика выполненных\отказанных заявок
+
+Пример заявки:
+{
+“request”: “Помыть слона”
+“bid”: 100.0
+“due_date”: “2018-09-30”
+}
+
+Требования к приложению:
+- Java 8
+- Spring Boot
+- Mysql\MongoDb
+- Jooq\Spring Data
+ - Tomcat\Jetty
+
+ */
 @SpringBootApplication
 public class ApplicationStart {
 
